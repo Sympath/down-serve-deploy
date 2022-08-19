21bffec5f4986e45bf9be0c643f28eb8647bb1ea
Wzyan <3101885298@qq.com>
2022-08-19 10:19:54 +0800

    kkb-down-deploy | 完成指南

---
+++ init.sh
@@ -0,0 +1,24 @@
+#!/usr/bin
+#  1. 配置 基础前端环境 git|node|pm2 环境
+#  1.1 安装nvm
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.0/install.sh | bash
+#  1.2 安装node
+nvm use stable
+#  1.3 安装pm2
+npm install pm2 -g
+#  2. 搭建 kkb-down-core 运行环境：ffmpeg 安装
+#  2.1 安装依赖包
+wget http://www.ffmpeg.org/releases/ffmpeg-3.1.tar.gz
+tar -zxvf ffmpeg-3.1.tar.gz
+cd ffmpeg-3.1
+./configure --prefix=/usr/local/ffmpeg --enable-shared --disable-debug  --disable-doc --enable-avresample --enable-nonfree --enable-gpl \
+--enable-openssl  --disable-avcodec  --disable-swresample --disable-avfilter 
+make && make install
+#  2.2 添加环境变量
+echo "export PATH=$PATH:/usr/local/ffmpeg/bin" >> /etc/profile
+source /ect/profile 
+#  3. 下拉 kkb-down-serve 仓库、安装依赖并 pm2 启动
+git clone git@github.com:Sympath/download-serve.git
+cd download-serve
+npm install
+npm run prd
---
+++ readme.md
@@ -0,0 +1,12 @@
+## 使用指南
+
+服务器快速部署能力 ==》 支持一个初始化的 linux 服务器直接执行脚本自动搭建 kkb-down-serve，及 kkb-down-core 运行环境
+适用于：centos
+
+1. 在服务器上下拉对应仓库
+2. 执行初始化脚本进行搭建
+
+```
+git clone git@github.com:Sympath/down-serve-deploy.git
+cd down-serve-deploy && nohup ./init.sh &
+```
