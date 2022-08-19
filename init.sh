5b8a2e80219a774f6de31fb258d82d3b091c6ad4
Wzyan <3101885298@qq.com>
2022-08-19 11:57:34 +0800

    kkb-down-deploy | 仓库重置处理

--- init.sh
+++ init.sh
@@ -8,8 +8,8 @@ npm install pm2 -g
 #  2. 搭建 kkb-down-core 运行环境：ffmpeg 安装
 #  2.1 安装依赖包
 wget http://www.ffmpeg.org/releases/ffmpeg-3.1.tar.gz
-tar -zxvf ffmpeg-3.1.tar.gz
-cd ffmpeg-3.1
+tar -zxvf ffmpeg-3.1.tar.gz  -C ../
+cd ../ffmpeg-3.1
 ./configure --prefix=/usr/local/ffmpeg --enable-shared --disable-debug  --disable-doc --enable-avresample --enable-nonfree --enable-gpl \
 --enable-openssl  --disable-avcodec  --disable-swresample --disable-avfilter 
 make && make install
